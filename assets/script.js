27bb9c6f9fcb4b07707c706051910d7a1728b6f1
CathKjeldahl <cath.kjeldahl@gmail.com>
2025-11-10 08:57:51 +0100

    update js

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,25 @@
-jQuery(document).ready(function($) {
-    var $box = $('#bellis-box');
+document.addEventListener('DOMContentLoaded', function() {
+    var box = document.getElementById('bellis-box');
 
     // Sørger for, at boksen starter skjult
-    $box.addClass('slide-top');
+    box.classList.add('slide-top');
 
     // Vent 2 sekunder før den glider ned
     setTimeout(function() {
-        $box.removeClass('slide-top').addClass('slide-down');
+        box.classList.remove('slide-top');
+        box.classList.add('slide-down');
     }, 2000);
 
     // Luk boksen
-    $('#bellis-close').on('click', function() {
-        $box.removeClass('slide-down').addClass('slide-top');
+    var closeButton = document.getElementById('bellis-close');
+    closeButton.addEventListener('click', function() {
+        box.classList.remove('slide-down');
+        box.classList.add('slide-top');
     });
 
     // Knapfunktion – sender brugeren videre til dit link
-    $('#bellis-button').on('click', function() {
+    var actionButton = document.getElementById('bellis-button');
+    actionButton.addEventListener('click', function() {
         window.location.href = 'https://storyscaping.shstudio.dk/elementor-615/home/';
     });
 });
